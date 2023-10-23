f5e1028abd1f215b62f22cefa2218e363e54260d
Edgar HAro <ehz.dev@gmail.com>
2023-10-22 22:43:46 -0600

    Modularización de la Vista Principal e Internacionalización Implementada"
    Se ha realizado una importante modularización de la vista principal. Se han separado las secciones del encabezado, menú y pie de página para mejorar la organización y mantenibilidad del código. Además, se ha implementado el soporte multilingüe habilitando archivos de recursos en español e inglés.

--- Axtra.Web/Program.cs
+++ Axtra.Web/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
@@ -23,5 +33,20 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    
+
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("es-MX")
+};
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("es-ES"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
 
 app.Run();
